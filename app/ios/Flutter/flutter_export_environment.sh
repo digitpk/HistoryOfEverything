86deaed5321736ac180e09368dbf529a227fc1e7
digitpk <webmaster@digit.com.pk>
2020-07-02 19:46:28 +0500

    my learning

---
+++ app/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\Abdul Majid\AppData\Local\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Abdul Majid\AndroidStudioProjects\HistoryOfEverything\app"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=C:\Users\Abdul Majid\AppData\Local\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.0.1"
+export "FLUTTER_BUILD_NUMBER=19"
